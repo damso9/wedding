92be688b4f736136917cac1cbdb385f1aa626d9d
Damilola Ayodele <ayodeled28@gmail.com>
2022-09-05 16:12:21 +0100

    added a new map

--- src/components/Info.js
+++ src/components/Info.js
@@ -41,22 +41,20 @@ export const Info = () => {
           </figure>
         </section>
 
-            <iframe
-style={{
-    width: "100%",
-    height:"350px",
-}}
-              id="gmap_canvas"
-              src="https://maps.google.com/maps?q=ASSEMBLIES%20OF%20GOD%20CHURCH,%20Pinnock%20Estate&t=&z=13&ie=UTF8&iwloc=&output=embed"
-              frameborder="0"
-              scrolling="no"
-              marginheight="0"
-              marginwidth="0"
-            ></iframe>
+        <iframe
+          style={{
+            width: "100%",
+            height: "350px",
+          }}
+          id="gmap_canvas"
+          src="https://maps.google.com/maps?q=6.453607,3.509043&t=&z=13&ie=UTF8&iwloc=&output=embed"
+          frameborder="0"
+          scrolling="no"
+          marginheight="0"
+          marginwidth="0"
+        ></iframe>
 
-            <br />
-
-
+        <br />
       </div>
     </>
   );
